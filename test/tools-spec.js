af0277f17339d05aeb1435637bb3397c643851ca
donkeykong91 <darylnator@gmail.com>
2018-07-05 13:52:23 -0700

    Nested describe function in another describe function for tools

--- test/tools-spec.js
+++ test/tools-spec.js
@@ -1,12 +1,17 @@
 var expect = require("chai").expect;
 var tools = require("../lib/tools");
 
-describe("printName()", function () {
-  it("should print the last name first", function () {
+describe("Tools", function () {
 
-    var results = tools.printName({first: "Alex", last: "Banks"});
+  describe("printName()", function () {
+    it("should print the last name first", function () {
 
-    expect(results).to.equal("Banks, Alex");
-    
+      var results = tools.printName({first: "Alex", last: "Banks"});
+
+      expect(results).to.equal("Banks, Alex");
+
+    });
+
   });
+
 });
