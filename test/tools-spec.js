eb1a989f4240ca7bed3ca3d5520100e0694c0f5a
donkeykong91 <darylnator@gmail.com>
2018-07-05 15:08:28 -0700

    Changed wait time to 5 seconds rather than default 2 seconds

--- test/tools-spec.js
+++ test/tools-spec.js
@@ -4,6 +4,7 @@ var tools = require("../lib/tools");
 describe("Tools", function () {
 
   describe("printName()", function () {
+    
     it("should print the last name first", function () {
 
       var results = tools.printName({first: "Alex", last: "Banks"});
@@ -15,6 +16,9 @@ describe("Tools", function () {
   });
 
   describe("loadWiki()", function () {
+
+    this.timeout(5000);
+
     it("Load Abraham Lincoln's wikipedia page", function (done) {
 
       tools.loadWiki({ first: "Abraham", last: "Lincoln"}, function (html) {
@@ -23,6 +27,7 @@ describe("Tools", function () {
       });
 
     });
+
   });
 
 });
