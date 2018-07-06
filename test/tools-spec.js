13a69e9b4043c245365d25bfcc1604cde01177e5
donkeykong91 <darylnator@gmail.com>
2018-07-06 06:04:08 -0700

    Added a nock (mock) server to represent wikipedia and test on that rather than the real wikipedia page

--- test/tools-spec.js
+++ test/tools-spec.js
@@ -1,10 +1,11 @@
 var expect = require("chai").expect;
 var tools = require("../lib/tools");
+var nock = require("nock");
 
 describe("Tools", function () {
 
   describe("printName()", function () {
-    
+
     it("should print the last name first", function () {
 
       var results = tools.printName({first: "Alex", last: "Banks"});
@@ -17,12 +18,18 @@ describe("Tools", function () {
 
   describe("loadWiki()", function () {
 
-    this.timeout(5000);
+    before(function () {
+
+      nock("https://en.wikipedia.org")
+        .get("/wiki/Abraham_Lincoln")
+        .reply(200, "Mock Abraham Lincoln Page");
+
+    });
 
     it("Load Abraham Lincoln's wikipedia page", function (done) {
 
       tools.loadWiki({ first: "Abraham", last: "Lincoln"}, function (html) {
-        expect(html).to.be.ok;
+        expect(html).to.equal("Mock Abraham Lincoln Page");
         done();
       });
 
